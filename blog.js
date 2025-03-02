32b04a67a60a85f56a9859e02aaa0177d6a0da82
Tohiduzzaman <mdtohiduzzaman7@gmail.com>
2025-03-02 10:14:43 +0600

    first commit

--- blog.js
+++ blog.js
@@ -1,8 +1,9 @@
 document.getElementById("goToBlog").addEventListener("click", function () {
-  window.location.href = "blog.html"; // Redirect to blog page
+  window.location.href = "blog.html";
 });
 
-
- document.getElementById("goToHome").addEventListener("click", function () {
-   window.location.href = "index.html"; // Redirect to index page
- });
+document.getElementById("goToHome").addEventListener("click", function () {
+  window.location.href = "index.html";
+  alert(dfghsdfg);
+  console.log(5515);
+});
