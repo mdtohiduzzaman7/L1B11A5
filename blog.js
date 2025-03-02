278e17acc333016a4b8e53bd5a659031a9fd767f
Tohiduzzaman <mdtohiduzzaman7@gmail.com>
2025-03-02 10:46:50 +0600

    ain section

--- blog.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("goToBlog").addEventListener("click", function () {
-  window.location.href = "blog.html";
-});
-
-document.getElementById("goToHome").addEventListener("click", function () {
-  window.location.href = "index.html";
-  alert(dfghsdfg);
-  console.log(5515);
-});
--- home.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("goToHome").addEventListener('click', function () {
-    window.location.replace("index.html");
-});
